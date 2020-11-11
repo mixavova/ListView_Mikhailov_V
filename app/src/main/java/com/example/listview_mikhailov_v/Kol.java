f27398670c3bc43ebbd3022d07c918990e15808c
mixavova <mixavova2007@mail.ru>
2020-11-11 19:36:59 +0300

    ListView

---
+++ app/src/main/java/com/example/listview_mikhailov_v/Kol.java
@@ -0,0 +1,14 @@
+package com.example.listview_mikhailov_v;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Kol extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kol);
+    }
+}
